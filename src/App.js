14a1b0933051e19f510e8a5e6b412c8939756fa6
sara <sarah_khoshaghebat@yahoo.com>
2024-09-09 11:36:37 +0330

    redesign and add package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Shop } from "./pages/shop/shop";
+import { Cart } from "./pages/cart/cart";
+import { Nav } from "./components/nav";
+import { ShopContextProvider } from "./context/shopContext";
+import{RestaurantInformation} from "./components/Restaurant information"
+import { Burger } from "./components/burger";
+import { Pizza } from "./components/pizza";
+import { Toast } from "./components/toast";
+import { Appetizer } from "./components/appetizer";
+import { Salad } from "./components/salad";
+import { Drink } from "./components/drink";
+import { Footer } from "./components/footer";
 
 function App() {
   return (
     <div className="App">
-     
+      <ShopContextProvider>
+        <Router>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Shop />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/restaurantInformation" element={<RestaurantInformation />}/>
+            <Route path="/burger" element={<Burger/>} />
+            <Route path="/pizza" element={<Pizza/>} />
+            <Route path="/toast" element={<Toast/>} />
+            <Route path="/salad" element={<Salad/>} />
+            <Route path="/appetizer" element={<Appetizer/>} />
+            <Route path="/drink" element={<Drink/>} />
+          </Routes>
+          <Footer/>
+        </Router>
+      </ShopContextProvider>
     </div>
   );
 }
